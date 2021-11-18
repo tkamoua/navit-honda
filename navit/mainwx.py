0af1b83e1ab2205d057b474258db75a69ad3580c
Mobility Collaboration Garage <you@example.com>
2021-11-18 18:39:14 -0500

    Alerts done!

--- navit/mainwx.py
+++ navit/mainwx.py
@@ -1,9 +1,12 @@
 from wxalerts import *
 import time
+import gpsd
 
-lat = 41.8
-lon = -78.64
+
+gpsd.connect()
 while(True):
+    packet = gpsd.get_current()
+    lat = packet.lat
+    lon = packet.lon
     time.sleep(5)
     get_alerts(lat,lon)
-    lat = lat+0.01
